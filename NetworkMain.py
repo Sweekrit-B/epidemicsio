60f358c986ca60bfdf612c5381092afd1744f073
Sweekrit-B <63749658+Sweekrit-B@users.noreply.github.com>
2024-07-23 22:10:56 -0700

    Update Network Model
    Increased customizability + age, genetic, and lifetime risk factors

--- NetworkMain.py
+++ NetworkMain.py
@@ -13,10 +13,10 @@ def network_portrayal(G):
                 color = 'green'
             elif agent.wealth == 0 and agent.recovered == 0:
                 color = 'blue'
-            else:
+            '''else:
                 color = 'grey'  # Add debugging here to print unexpected conditions
                 print(f"Agent {agent.unique_id} has unexpected wealth={agent.wealth} and recovered={agent.recovered}")
-
+            '''
             portrayal['nodes'].append({
                 'id': node_id,
                 'size': 6,
@@ -25,18 +25,27 @@ def network_portrayal(G):
             })
 
     portrayal['edges'] = [
-        {'source': source, 'target': target, 'color': 'grey'} for source, target in G.edges
+        {'source': source, 'target': target, 'color': 'black'} for source, target in G.edges
     ]
 
     return portrayal
 
-number_of_agents_slider = \
-    mesa.visualization.Slider('Number of Agents', 1, 1, 500, 10)
-chance_of_infection_slider = \
-    mesa.visualization.Slider('Chance of Infection', 0, 0, 10, 1)
+number_of_agents_slider = mesa.visualization.NumberInput('Number of Agents', 40, description = "Number of agents in the simulation.")
+chance_of_infection_slider = mesa.visualization.NumberInput('Implicit Chance of Infection (0 < x < 100)', 30, description="Chance of getting infected per interaction for an agent that does not have any factors that increase their chance of infection.")
+
+age_risk = mesa.visualization.NumberInput('At-Risk due to Age (0 < x < 100)', 30,description="Proportion of population that is at-risk due to their age.")
+genetic_risk = mesa.visualization.NumberInput('At-Risk due to Genetics (0 < x < 100)', 30,description="Proportion of population that is at-risk due to their genetics.")
+lifestyle_risk = mesa.visualization.NumberInput('At-Risk due to Lifestyle (0 < x < 100)', 30,description="Proportion of population that is at-risk due to their lifestyle.")
 
 
-grid = mesa.visualization.NetworkModule(network_portrayal, 500, 500)
+graph_type_choice = mesa.visualization.Choice('Graph Type', value='Barabasi Albert', choices=['Barabasi Albert', 'Watts Strogatz', 'Erdos Renyi', 'Power Law Cluster'], description="Barabási-Albert for scale-free networks (power law degree distribution) with hubs. \nWatts-Strogatz for small-world networks with short paths and high clustering. \nErdős-Rényi for random graphs with uniform connectivity. \nPower Law Cluster for networks with both high clustering using triangles and a power-law degree distribution.")
+
+p_value_slider = mesa.visualization.NumberInput('P Value (0 < x < 10)', 4, description="Probability of rewiring each edge (Watts Strogatz). \nProbability of an edge existing between a pair of nodes (Erdos Renyi). \nProbability of a triangle being created (Power Law Cluster).")
+m_value_slider = mesa.visualization.NumberInput('M Value (0 < x < number of nodes)', 3, description="Number of new edges per node, selected preferentially (Barabasi Albert and Power Law Cluster")
+num_recoveries_slider = mesa.visualization.NumberInput('Number of Recoveries', 3, description="Number of recoveries an agent has before they are immune.")
+num_steps_to_recovery_slider = mesa.visualization.NumberInput('Number of Steps till Recovery', 10, description="Number of steps an agent has to take before they automatically recover")
+
+grid = mesa.visualization.NetworkModule(network_portrayal, canvas_height=1000, canvas_width=864)
 chart = mesa.visualization.ChartModule([{'Label': 'Prevalence', 'Color': 'Black'}], data_collector_name="datacollector")
 chart2 = mesa.visualization.ChartModule([{'Label': 'Incidence', 'Color': 'Blue'}], data_collector_name="datacollector")
 combined_chart = mesa.visualization.ChartModule(
@@ -53,9 +62,15 @@ server = mesa.visualization.ModularServer(
     [grid, chart, chart2, combined_chart],
     "epicurves.io - Network Simulation",
     {'N': number_of_agents_slider,
-     'chance_of_infection': chance_of_infection_slider,
-     'avg_node_degree': 3}
+     'chance_of_infection': chance_of_infection_slider, 'age_risk_proportion': age_risk,
+     'genetic_risk_proportion': genetic_risk,
+     'lifestyle_risk_proportion': lifestyle_risk,
+     'graph_type': graph_type_choice,
+     'p_value': p_value_slider,
+     'm_value': m_value_slider,
+     'num_recoveries_for_immune': num_recoveries_slider,
+     'num_steps': num_steps_to_recovery_slider}
 )
 
-server.port = 8659
+server.port = 8660
 server.launch()
--- NetworkModel.py
+++ NetworkModel.py
@@ -8,20 +8,17 @@ import matplotlib
 import networkx as nx
 
 matplotlib.use('TkAgg')
+
 def compute_prevalence(model):
     return sum(1 for agent in model.schedule.agents if agent.wealth == 1 and agent.recovered == 0)/model.num_nodes
-    
 def compute_incidence(model):
     return model.new_cases / model.num_nodes
-
 def compute_recovered(model):
     return sum(1 for agent in model.schedule.agents if agent.recovered == 1)
-
 def compute_infected(model):
     return sum(1 for agent in model.schedule.agents if agent.wealth == 1 and agent.recovered == 0)
-
 def compute_susceptible(model):
-    return sum(1 for agent in model.schedule.agents if agent.wealth == 0 and agent.recovered == 0)
+    return sum(1 for agent in model.schedule.agents if agent.wealth == 0 and agent.num_recoveries < model.num_recoveries_for_immune)
 
 class NetworkAgent(mesa.Agent):
     def __init__(self, unique_id, model):
@@ -31,13 +28,23 @@ class NetworkAgent(mesa.Agent):
         self.wealth = 0
         self.steps = 0
         self.recovered = 0
+        self.num_recoveries = 0
+
+        self.increase_age_risk = 1
+        self.increase_genetic_risk = 1
+        self.increase_lifestyle_risk = 1
+
         id_list = []
         id_list.append(unique_id)
         for x in id_list:
             if x < 1:
                 self.wealth = 1
-            else:
-                self.wealth = 0
+            if random.random() < self.model.age_risk_proportion/100:
+                self.increase_age_risk = 1.2
+            if random.random() < self.model.genetic_risk_proportion/100:
+                self.increase_genetic_risk = 1.2
+            if random.random() < self.model.lifestyle_risk_proportion/100:
+                self.increase_lifestyle_risk = 1.2
 
     def move(self):
         possible_steps = self.model.grid.get_neighborhood(self.pos, include_center=False, radius=2)
@@ -50,8 +57,9 @@ class NetworkAgent(mesa.Agent):
         susceptible_neighbors = [agent for agent in neighbor_agents if agent.wealth == 0]
         if susceptible_neighbors:
             for a in susceptible_neighbors:
-                if a.wealth == 0 and a.recovered != 1:
-                    if a.random.random() < a.model.chance_of_infection/10:
+                if a.wealth == 0 and a.num_recoveries < a.model.num_recoveries_for_immune:
+                    if a.random.random() < a.model.chance_of_infection/100 * a.increase_age_risk * a.increase_genetic_risk * a.increase_lifestyle_risk:
+                        a.recovered = 0
                         a.wealth = 1
                         self.model.new_cases += 1
                         break
@@ -61,27 +69,44 @@ class NetworkAgent(mesa.Agent):
         if self.wealth == 1:
             self.give_money()
             self.steps += 1
+            print(f"Agent {self.unique_id} has stepped {self.steps} times.")
         elif self.wealth == 0:
             self.steps = 0
-        if self.steps == 14:
-            self.wealth -= 1
+        if self.steps == self.model.num_steps:
+            self.steps = 0
+            self.wealth = 0
             self.recovered = 1
-            self.model.new_recoveries += 1
+            self.num_recoveries += 1
+            print(f"Agent {self.unique_id} has stepped twice.")
+            print(f"Agent {self.unique_id} has {self.num_recoveries}")
 
 
 class NetworkModel(mesa.Model):
     """A model with some number of agents."""
 
-    def __init__(self, N, chance_of_infection, avg_node_degree):
+    def __init__(self, N, chance_of_infection, graph_type, m_value, p_value, num_recoveries_for_immune, num_steps,  age_risk_proportion, genetic_risk_proportion, lifestyle_risk_proportion):
         super().__init__()
         self.num_nodes = N
-        prob = avg_node_degree / self.num_nodes
-
+        self.num_steps = num_steps
+        self.num_recoveries_for_immune = num_recoveries_for_immune
         self.chance_of_infection = chance_of_infection
+        self.age_risk_proportion = age_risk_proportion
+        self.genetic_risk_proportion = genetic_risk_proportion
+        self.lifestyle_risk_proportion = lifestyle_risk_proportion
         self.schedule = mesa.time.RandomActivation(self)
-        
         #Here, need to generate a random graph, and then make a grid on it
-        self.G = nx.erdos_renyi_graph(n=self.num_nodes, p=prob)
+        if graph_type == 'Barabasi Albert':
+            #In a Barabasi Albert graph, the m value is the number of new edges per new node. Therefore, when a new node is added to the network, it establishes m edges to existing nodes. Nodes with more connections are more likely to receive new edges. A larger m value will increase the connectivity of new nodes, leading to a network with a high number of edges and more pronounced hubs.
+            self.G = nx.barabasi_albert_graph(n=self.num_nodes, m=m_value)
+        elif graph_type == 'Watts Strogatz':
+            #In a Watts Strogatz graph, the p_value represents the probability of rewiring each edge, or the randomness of the graph. It controls the tradeoff between a regular lattice (p = 0), and a completely random graph (p = 1)
+            self.G= nx.watts_strogatz_graph(n=self.num_nodes, k=6, p=p_value/10)
+        elif graph_type == 'Erdos Renyi':
+            #In an Erdos-Renyi graph, the p_value represents the probability that an edge exists between any pair of nodes. This probability controls the density of the network, with a p = 0 creating a graph with n isolated nodes and no edges, while a p = 1 creates a complete graph where everyr possible edge between nodes exists.
+            self.G = nx.erdos_renyi_graph(n=self.num_nodes, p=p_value/10)
+        elif graph_type == 'Power Law Cluster':
+            #In a Power-Law Cluster Graph, m is the number of new edges per node (the same as a Barabasi Albert graph). Meanwhile, p is the probability of adding a triangle, or when a new edge is added to the network, a triangle forming by adding an additional edge between two existing nodes that are both connected to the new node. A higher p value means higher clustering.
+            self.G = nx.powerlaw_cluster_graph(n=self.num_nodes, m=m_value, p=p_value/10)
         self.grid = mesa.space.NetworkGrid(self.G)
 
         self.new_cases = 0
